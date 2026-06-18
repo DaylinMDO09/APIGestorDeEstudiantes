3fd5e269e6100498ed7e8359bd261afec526c4b0
Daylin Monte De Oca <daylinmf09@gmail.com>
2026-06-18 19:35:19 -0400

    Primer commit

---
+++ APIestudiantes/Controllers/EstudiantesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIestudiantes.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ APIestudiantes/Modelos/EstudianteModel.cs
@@ -0,0 +1,14 @@
+﻿namespace APIestudiantes.Modelos
+{
+    public class EstudianteModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public string Carrera { get; set; }
+        public int Edad { get; set; }
+        public decimal Promedio { get; set; }
+        public bool Activo { get; set; }
+    }
+}
